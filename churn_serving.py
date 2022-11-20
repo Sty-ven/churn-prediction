1c44de8e841a4342366b22bec3e9867d5dad0652
Sty-ven <oriyomistephen@gmail.com>
2022-11-20 09:49:55 +0100

    first commit

---
+++ churn_serving.py
@@ -0,0 +1,47 @@
+# import necessary packages
+
+import pickle
+import numpy as np
+
+from flask import Flask, request, jsonify
+
+# create function for preduction
+
+
+
+
+def predict_single(customer, dv, model):
+    X = dv.transform([customer])
+    y_pred = model.predict_proba(X)[:, 1]
+    return y_pred[0]
+
+
+# open the saved model
+
+with open('churn_model.bin', 'rb') as f_in:
+    dv, model = pickle.load(f_in)
+
+
+
+
+app = Flask('churn')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    customer = request.get_json()
+
+    prediction = predict_single(customer, dv, model)
+    churn = prediction >= 0.5
+    
+    result = {
+        'churn_probability': float(prediction),
+        'churn': bool(churn),
+    }
+
+    return jsonify(result)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=9696)
+
---
+++ flask_test.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+# create a flask app
+app = Flask('test')
+
+# assigning the app to an address
+
+@app.route('/ping', methods=['GET'])
+def ping():
+    return "PONG"
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=9696)
+
